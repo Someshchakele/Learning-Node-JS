19e42be5e090315788d0c3542bc963bb89828953
Somesh Chakele <77913438+Someshchakele@users.noreply.github.com>
2025-01-05 21:13:54 +0530

    Read and Write Files synchronously

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const readline =require('readline')
-const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,
-});
+const fs = require('fs');
+// const rl = readline.createInterface({
+//     input:process.stdin,
+//     output:process.stdout,
+// });
 
-rl.question('Please enter yo name:' , (name)=>{
-    console.log("You entered :" + name)
-rl.close();
-})
+// rl.question('Please enter yo name:' , (name)=>{
+//     console.log("You entered :" + name)
+// rl.close();
+// })
 
-rl.on('close', ()=> {
-console.log(" interface Close");
-process.exit(0);
-})
+// rl.on('close', ()=> {
+// console.log(" interface Close");
+// process.exit(0);
+// })
+
+//Lecture 4
+
+let textIn = fs.readFileSync('./Files/input.txt', 'utf-8');
+console.log(textIn);
+
+let content = `Data read from input.txt: ${textIn}. \n Date created ${new Date()}`
+fs.writeFileSync('./Files/output.txt', content)
