f2f7e90fb4e084a367d9afb29c8baff2f29e65bf
Somesh Chakele <77913438+Someshchakele@users.noreply.github.com>
2025-01-05 20:55:03 +0530

    Created terminal realine interface

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+const readline =require('readline')
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+rl.question('Please enter yo name:' , (name)=>{
+    console.log("You entered :" + name)
+rl.close();
+})
+
+rl.on('close', ()=> {
+console.log(" interface Close");
+process.exit(0);
+})
