8c369500266f430ea5efd357242eabdea0720fb2
Somesh Chakele <77913438+Someshchakele@users.noreply.github.com>
2025-01-06 21:51:14 +0530

    Read write files asynchronously

--- app.js
+++ app.js
@@ -17,8 +17,27 @@ const fs = require('fs');
 
 //Lecture 4
 
-let textIn = fs.readFileSync('./Files/input.txt', 'utf-8');
-console.log(textIn);
+// let textIn = fs.readFileSync('./Files/input.txt', 'utf-8');
+// console.log(textIn);
 
-let content = `Data read from input.txt: ${textIn}. \n Date created ${new Date()}`
-fs.writeFileSync('./Files/output.txt', content)
+// let content = `Data read from input.txt: ${textIn}. \n Date created ${new Date()}`
+// fs.writeFileSync('./Files/output.txt', content)
+
+
+//Lecture 5 - Theory
+
+//Lecture 6 - Theory
+
+//Lecture 7 - Read & Write Files Asynchronously
+
+fs.readFile('./Files/start.txt', 'utf-8' , (error1 , data1)=>{
+    console.log(data1)
+    fs.readFile(`./Files/${data1}.txt`, 'utf-8' , (error2 , data2)=>{
+        console.log(data2)
+            fs.writeFile('./Files/output.txt', `${data1}`        
+            )
+        
+    })
+})
+
+console.log("Reading Files...")
