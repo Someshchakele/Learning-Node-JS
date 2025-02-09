d12091038448d53d25ce9c069d792468a6df94fc
Somesh Chakele <77913438+Someshchakele@users.noreply.github.com>
2025-02-09 15:34:01 +0530

    36 API: Handling Route Parameters

--- app.js
+++ app.js
@@ -170,34 +170,58 @@ let app = express();
 let movies = JSON.parse(fs.readFileSync('./Data/products.json'))
 app.use(express.json())
 
-app.get('/api/v1/movies', (req , res)=>{
-    res.status(200).json(
-        {
-            status:"success",
-            count:movies.length,
-            data:{
-                movies:movies
-            }
+// app.get('/api/v1/movies', (req , res)=>{
+//     res.status(200).json(
+//         {
+//             status:"success",
+//             count:movies.length,
+//             data:{
+//                 movies:movies
+//             }
+//         }
+//     );
+// });
+
+app.get('/api/v1/movies/:id', (req , res)=>{
+    // console.log(req.params);
+
+    const id = req.params.id * 1;
+
+    let movie = movies.find(el => el.id === id);
+
+    if(!movie){
+       return res.status(404).json({
+            status:"error",
+            message:"failed"
+        });
+    }
+
+    res.status(200).json({
+        status:"success",
+        data:{
+            movie:movie
         }
-    );
-});
-
-app.post('/api/v1/movies', (req , res)=>{
-    // console.log(req.body)
-    const newId = movies[movies.length - 1].id + 1
-    const newMovie = Object.assign({id: newId}, req.body)
-
-    movies.push(newMovie);
-    fs.writeFile('./Data/products.json', JSON.stringify(movies), (err)=>{
-        res.status(201).json({
-            status: "status",
-            data: {
-                movie:newMovie
-            }
-        })
-    })
-   
+    });
 })
+
+// app.post('/api/v1/movies', (req , res)=>{
+//     // console.log(req.body)
+//     const newId = movies[movies.length - 1].id + 1
+//     const newMovie = Object.assign({id: newId}, req.body)
+
+//     movies.push(newMovie);
+//     fs.writeFile('./Data/products.json', JSON.stringify(movies), (err)=>{
+//         res.status(201).json({
+//             status: "status",
+//             data: {
+//                 movie:newMovie
+//             }
+//         })
+//     })
+   
+// })
+
+
 const port = 3000;
 app.listen(port, ()=>{
     console.log('server has started...')
