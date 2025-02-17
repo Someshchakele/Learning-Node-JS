5dbc16c3a386fc96a6daa0427634cd7ca1ba1e75
Somesh Chakele <77913438+Someshchakele@users.noreply.github.com>
2025-02-17 23:31:45 +0530

    Creating Schema & Model

--- server.js
+++ server.js
@@ -14,6 +14,21 @@ mongoose.connect(process.env.CONN_STR,{
 }).catch((error)=>{
     console.log("Some error occured")
 })
+
+const movieSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:[true , 'Name is required '],
+        unique: true
+
+    },
+    description: String,
+    duration: {
+        type: Number,
+        required:[true , 'Duration is required ']
+    },
+    ratings:{type: Number , default:1.0}
+})
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log('server has started...')
