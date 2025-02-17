2cbd098bd530f8622dc968a114130c8eef262073
Somesh Chakele <77913438+Someshchakele@users.noreply.github.com>
2025-02-17 23:25:20 +0530

    Connect to remote Database

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
 const app = require('./app')
+
+dotenv.config({path:'./config.env'});
 
 console.log("env",process.env)
 console.log(app.get('env'));
-const port = 3000;
+
+mongoose.connect(process.env.CONN_STR,{
+    useNewUrlParser: true
+}).then((conn)=>{
+    console.log("DB Connection Successful")
+}).catch((error)=>{
+    console.log("Some error occured")
+})
+const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log('server has started...')
 })
