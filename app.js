d17f27af745261ae186067c219369e7d0843999f
Somesh Chakele <77913438+Someshchakele@users.noreply.github.com>
2025-03-05 23:39:03 +0530

    92 Creating a Custom Error class

--- app.js
+++ app.js
@@ -166,12 +166,14 @@
 const express = require('express');
 
 const moviesRouter = require('./Routes/moviesRoutes');
-
+const CustomError = require('./Utils/customErrors');
+const globalerrorHandler = require('./Controller/errorController');
 
 let app = express();
 
 app.use(express.json())
 app.use(express.static('./public'))
+
 
 
 
@@ -188,23 +190,14 @@ app.all('*', (req, res , next)=>{
     //     status: 'fail',
     //     message: `Can't find ${req.originalUrl} on this server`
     // })
-    const err = new Error(`Can't find ${req.originalUrl} on this server`)
-    err.status = 'fail';
-    err.statusCode = 404;
-
+    // const err = new Error(`Can't find ${req.originalUrl} on this server`)
+    // err.status = 'fail';
+    // err.statusCode = 404;
+    const err = new CustomError(`Can't find ${req.originalUrl} on this server`, 404)
     next(err)
 })
 
-app.use((err, req, res, next)=>{
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.statusCode,
-        message: err.message
-    })
-}
-)
+app.use(globalerrorHandler)
 
 
 module.exports = app;
