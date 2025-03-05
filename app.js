25c2087bbd00e5509c15508486750cfffcdd7fe2
Somesh Chakele <77913438+Someshchakele@users.noreply.github.com>
2025-03-05 22:50:56 +0530

    91 Global Error Handling Middleware

--- app.js
+++ app.js
@@ -183,12 +183,28 @@ app.use(express.static('./public'))
 
 
 app.use('/api/v1/movies', moviesRouter)
-app.all('*', (req, res)=>{
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server`
-    })
+app.all('*', (req, res , next)=>{
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server`
+    // })
+    const err = new Error(`Can't find ${req.originalUrl} on this server`)
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    next(err)
 })
+
+app.use((err, req, res, next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.statusCode,
+        message: err.message
+    })
+}
+)
 
 
 module.exports = app;
