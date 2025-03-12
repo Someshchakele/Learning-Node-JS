4b2ba6e93c45028148ab0e94f7cc11def00e9bde
Somesh Chakele <77913438+Someshchakele@users.noreply.github.com>
2025-03-13 00:21:52 +0530

    106 Signing up a User | Authentication & Authorization

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const User = require('../Models/userModel');
+const jwt = require('jsonwebtoken');
 
 exports.signup = async (req, res, next) => {  
   const newUser = await User.create(req.body);
+
+  const token = jwt.sign({id: newUser._id}, process.env.SECRET_STR,{expiresIn :process.env.LOGIN_EXPIRES})
     res.status(201).json({  
         status: 'success',
+        token,
         data: {
             user: newUser
         }
